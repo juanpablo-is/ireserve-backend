3ed5e0198ecfb5e10d1d149cc80e133272202a36
cristian-avendano <cristian-avendano@upc.edu.co>
2021-04-25 22:05:26 -0500

    IReserve-17 Servicio para formulario registrar menú

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,24 +1,42 @@
 const db = require("../utils/database");
 
 const getMenu = async (req, res) => {
-    const snapshot = await db.collection('menu').get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    });
-    res.json(snapshot);
+    const id = req.query.idUser;
+    const snapshot = await db.collection('menu')
+    .where('idUser','==',id)
+    .get();
+    if (snapshot.docs.length>0) {
+        res.status(201).json(snapshot.docs[0].data());
+    } else {
+        res.status(201).json({response:'empty'});
+    }
 };
 
 const createMenu = async (req, res) => {
-    const menu = await db.collection('menu').add({
-        'first': 'Alan',
-        'middle': 'Mathison',
-        'last': 'Turing',
-        'born': 1912
-    });
-    res.json(menu);
-};
+    const menu = req.body;
+    console.log(menu);
+    if(menu){
+        const snap = await db.collection('users')
+        .where('email', '==', menu.idUser)
+        .get()
 
-module.exports = {
-    getMenu,
-    createMenu
+        const email = snap.docs[0].data().email;
+
+        const auxmenu = await db.collection('menu')
+        .where('idUser', '==', email)
+        .get()
+
+        if (auxmenu.docs.length > 0) {
+            db.collection('menu')
+            .doc(auxmenu.docs[0].id)
+            .update(menu);
+            res.json({res:"Menu Actualizado"})
+        }else{
+            db.collection('menu')
+            .add(menu)
+            res.json({res:"Menu Agregado"})
+        }
+    }else{
+        res.status(201).json({res:"vacio"})
+    }
 };
--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 // Controladores
 const menuController = require('../controllers/menu');
 const restaurantController = require('../controllers/restaurant');
-
 /**
  * Rutas REST para menu.
  */
 router.get('/menu', menuController.getMenu);
-router.post('/menu', menuController.getMenu);
-
+router.post('/menu', menuController.createMenu);
 /**
  * Rutas REST para restaurante.
  */
 router.get('/restaurant', restaurantController.getRestaurants);
 router.post('/restaurant', restaurantController.createRestaurant);
-
-module.exports = router;
